017da23f8513451aa675300c4f39e180ae6ceb2b
IT-Marika <marika@skov-revision.dk>
2025-03-17 14:31:26 +0100

    Update chat-menu-header.tsx

--- src/features/chat-page/chat-menu/chat-menu-header.tsx
+++ src/features/chat-page/chat-menu/chat-menu-header.tsx
@@ -1,10 +1,14 @@
 import { CreateChatAndRedirect } from "../chat-services/chat-thread-service";
 import { ChatContextMenu } from "./chat-context-menu";
 import { NewChat } from "./new-chat";
+import { Logo, LogoImage } from "../ui/logo";
 
 export const ChatMenuHeader = () => {
   return (
     <div className="flex p-2 px-3 justify-end">
+      <Logo className="h-12 w-80">
+        <LogoImage src={"ai-icon.png"} />
+      </Logo>
       <form action={CreateChatAndRedirect} className="flex gap-2 pr-3">
         <NewChat />
         <ChatContextMenu />
