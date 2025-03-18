252553d8c0bd9ee7dfe9fe95b50e6f830f350cd6
IT-Marika <marika@skov-revision.dk>
2025-03-18 10:43:57 +0100

    Update chat-menu-header.tsx

--- src/features/chat-page/chat-menu/chat-menu-header.tsx
+++ src/features/chat-page/chat-menu/chat-menu-header.tsx
@@ -4,8 +4,9 @@ import { NewChat } from "./new-chat";
 
 export const ChatMenuHeader = () => {
   return (
-    <img src={'/danrevi-logo.svg'} width="100" padding-right="20" />
-    <div className="flex p-2 px-3 justify-end">  
+    
+    <div className="flex p-2 px-3 justify-center">  
+      <img src={'/danrevi-logo.svg'} width="130" />
       <form action={CreateChatAndRedirect} className="flex gap-2 pr-3">
         <NewChat />
         <ChatContextMenu />
