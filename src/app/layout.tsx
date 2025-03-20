38361b629c1f916d8fc7aea843425f18cdeaf527
IT-Marika <marika@skov-revision.dk>
2025-03-20 09:41:33 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,6 @@ export default function RootLayout({
       <body
         className={cn(inter.className, "h-full w-full flex  bg-background")}
       >
-        <h1>Værdifuld viden om økonomi</h1>
         <ThemeProvider
           attribute="class"
           defaultTheme="light"
@@ -34,7 +33,6 @@ export default function RootLayout({
           {children}
           <Toaster />
         </ThemeProvider>
-      <img src={'/danrevi-logo.svg'} width="500" />
       </body>
     </html>
   );
