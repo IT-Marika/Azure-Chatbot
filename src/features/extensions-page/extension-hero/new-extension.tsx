423f594290b0b7697b8300dccc5a409397e565cb
IT-Marika <marika@skov-revision.dk>
2025-03-21 10:50:02 +0100

    Update new-extension.tsx

--- src/features/extensions-page/extension-hero/new-extension.tsx
+++ src/features/extensions-page/extension-hero/new-extension.tsx
@@ -5,8 +5,8 @@ import { extensionStore } from "../extension-store";
 export const NewExtension = () => {
   return (
     <HeroButton
-      title="New Extension"
-      description="Create a new extension with your own internal API"
+      title="Ny Extension"
+      description="Opret en ny udvidelse med din egen interne API"
       icon={<PocketKnife />}
       onClick={() => extensionStore.newAndOpenSlider()}
     />
